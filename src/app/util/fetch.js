446af39c0e023ff2f0cefb4d12c3f3e229bb6183
Avner Peled <avnerus@gmail.com>
2017-11-17 18:24:20 +0100

    Support iOS 10.0 witout fetch

--- src/app/util/fetch.js
+++ src/app/util/fetch.js
@@ -4,23 +4,50 @@ class FetchUtil {
     }
 
     postJSON(target, data, accessToken) {
-        let options = {
-            method: 'POST',
-            credentials: 'include',
-            body: JSON.stringify(data),
-            headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+        if (typeof(fetch) != 'undefined') {
+            let options = {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify(data),
+                headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                }
             }
-        }
-        if (accessToken) {
-            options.headers.Authorization = accessToken;
-        }
+            if (accessToken) {
+                options.headers.Authorization = accessToken;
+            }
 
-        return fetch(target,options)
-        .then((response) => {
-             return response.json();
-        })
+            return fetch(target,options)
+            .then((response) => {
+                 return response.json();
+            })
+        } else if (IS_CLIENT && $) {
+            return new Promise((resolve, reject) => {
+                let options =  {
+                      type: "POST",
+                      url: target,
+                      data: JSON.stringify(data),
+                      dataType: 'json',
+                      contentType: 'application/json',
+                      success: (result) => {
+                          console.log("Jquery ajax result!", result);
+                          resolve(result);
+                      },
+                      error: (err) => {
+                          console.log("Jquery ajax error!", err);
+                          reject(err)
+                      }
+                };
+                if (accessToken) {
+                    options.headers = {Authorization : accessToken}
+                }
+
+                $.ajax(options);
+            });
+        } else {
+            throw new Error("No fetch client!");
+        }
     }
 
     get(target, accessToken) {
