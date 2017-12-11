beba9134517043817929958e13b30d1c6752bc95
Avnerus <avnerus@gmail.com>
2017-12-11 22:38:59 +0200

    relative dirname

--- src/server/app.js
+++ src/server/app.js
@@ -84,7 +84,7 @@ const SUPPORTED_LANGS = {
 const MongoStore = connectMongo(session);
 
 const app = feathers()
-.set('views', __dirname + "/src/server/views")
+.set('views', __dirname + "/views")
 .set('view engine', 'ejs')
 .configure(rest())
 .configure(socketio({wsEngine: 'uws'}))
@@ -92,7 +92,7 @@ const app = feathers()
 .use(compress())
 .options('*', cors())
 .use(cors())
-.use(feathers.static(__dirname + "/public"))
+.use(feathers.static(__dirname + "../../public"))
 .use(cookieParser())
 .use(bodyParser.json())
 .use(bodyParser.urlencoded({ extended: true  }))
