a9d1d5b5881b7d57a4c113bf0b5660cbe45f5024
Avnerus <avnerus@gmail.com>
2018-01-04 15:45:09 +0200

    dummy prompts in all languages

--- src/server/models/dummy-alarms.js
+++ src/server/models/dummy-alarms.js
@@ -21,26 +21,26 @@ export default [
             },
             fr: {
                 paragraphs : [ 
-                    "Avner is very much his own person, full of beautiful eccentricities. His savviness and wit garner him a lot of attention.", 
-                    "He has a penchant for challenging authority and abandoning convention. A desire for change and progress drives many of his actions."
+                    "Avner est une personne unique et amusante. Il est non-conventionnel et trace son propre chemin. Vous ne trouverez pas deux  dans le monde.", 
+                    "Il a tendance à questionner l'autorité et à faire fi des conventions. Un désir de changement et de progrès motive plusieurs de ses actions."
                 ],
                 instructions : [ 
-                    "Remind him how much the world needs his unique and sharp-witted sensibility.", 
-                    "Share how much you admire his audacity to breathe new life into tired conventions and make for a brighter future.", 
-                    "Reassure him that this is a noble task, and he deserves a break and a lot of recognition.", 
-                    "Bring him into the new day with confidence that he can do things his way today."
+                    "Encouragez-le à être fier de son excentricité et à sculpter cette journée comme il l'imagine.", 
+                    "Dites-lui combien vous admirez l'audace avec laquelle il apporte un vent de fraîcheur dans les conversations stagnantes et sa capacité à rêver d'un monde meilleur.", 
+                    "Assurez-lui qu'il s'agit là d'une noble tche, et qu'il mérite une pause ainsi que beaucoup de reconnaissance.", 
+                    "Offrez-lui une occasion de se recentrer avant de partir conquérir la journée et le monde."
                 ]
             },
             de: {
                 paragraphs : [ 
-                    "Avner is very much his own person, full of beautiful eccentricities. His savviness and wit garner him a lot of attention.", 
-                    "He has a penchant for challenging authority and abandoning convention. A desire for change and progress drives many of his actions."
+                    "Avner ist eine amüsante Person. Er ist unkonventionell und hat seine eigenen Ansichten. Jemanden wie Avner findet man nicht alle Tage.", 
+                    "Er fordert gern Autoritäten heraus und kümmert sich wenig um Gepflogenheiten. Er ist getrieben von einem Wunsch nach Wandel und Fortschritt."
                 ],
                 instructions : [ 
-                    "Remind him how much the world needs his unique and sharp-witted sensibility.", 
-                    "Share how much you admire his audacity to breathe new life into tired conventions and make for a brighter future.", 
-                    "Reassure him that this is a noble task, and he deserves a break and a lot of recognition.", 
-                    "Bring him into the new day with confidence that he can do things his way today."
+                    "Unterstützen Sie ihn in seinen Marotten und dabei, den Tag so zu gestalten, wie er es wünscht.", 
+                    "Zeigen Sie ihm, wie sehr Sie seine Fähigkeit bewundern, erlahmende Diskussionen zu beleben und auf eine bessere Zukunft hinzuwirken.", 
+                    "Versichern Sie ihm, dass das eine ehrenwerte Aufgabe ist und er dafür eine Pause und Anerkennung verdient.", 
+                    "Vermitteln Sie ihm Ausgeglichenheit, bevor er ins Leben hinausgeht, um die Welt zu erobern."
                 ]
             }
         },
@@ -80,26 +80,26 @@ export default [
             },
             fr: {
                 paragraphs : [ 
-                    "Rebecca is a dignified soul with an ability to bring sophisticated ideas and execute to perfection.", 
-                    "She is a cautious person. Weighing all the given outcomes before acting, people describe Rebecca as risk-averse."
+                    "Rebecca est une personne fiable et responsable qui prend des décisions et pose des gestes réfléchis.", 
+                    "Elle est animée par un fort sens du devoir. Même lorsque ça complique les choses, elle obéit aux règlements et remplit ses obligations."
                 ],
                 instructions : [ 
-                    "Remind her how capable she is and encourage her to keep doing whatever she's doing - because it's working!", 
-                    "Let her know how much respect you have for her deliberate approach to making decisions.", 
-                    "Reassure her that it's okay to take a small risk from time to time.", 
-                    "Make her feel confident in asserting herself today."
+                    "Rappelez-lui que les autres ont foi en elle et qu'elle mérite pleinement leur confiance.", 
+                    "Dites-lui combien son sens du devoir vous impressionne.", 
+                    "Rassurez-la en lui disant qu'il faut bien relaxer et s'amuser de temps en temps.", 
+                    "Donnez-lui confiance en sa capacité à communiquer avec les autres."
                 ]
             },
             de: {
                 paragraphs : [ 
-                    "Rebecca is a dignified soul with an ability to bring sophisticated ideas and execute to perfection.", 
-                    "She is a cautious person. Weighing all the given outcomes before acting, people describe Rebecca as risk-averse."
+                    "Rebecca ist zuverlässig und verantwortungsvoll und handelt mit Bedacht.", 
+                    "Sie ist sehr pflichtbewusst. Auch wenn es unangenehm ist, hält sie sich an die Regeln und an Vereinbarungen."
                 ],
                 instructions : [ 
-                    "Remind her how capable she is and encourage her to keep doing whatever she's doing - because it's working!", 
-                    "Let her know how much respect you have for her deliberate approach to making decisions.", 
-                    "Reassure her that it's okay to take a small risk from time to time.", 
-                    "Make her feel confident in asserting herself today."
+                    "Erinnern Sie sie daran, dass ihr andere vertrauen und sie ihr Vertrauen verdient.", 
+                    "Zeigen Sie ihr, wie sehr Sie ihr Pflichtbewusstsein beeindruckt.", 
+                    "Versichern Sie ihr, dass es in Ordnung ist, sich manchmal zu entspannen und das Leben zu genießen.", 
+                    "Geben Sie ihr zu verstehen, dass sie in der Lage ist, sich mit anderen zusammenzuschließen."
                 ]
             }
         },
@@ -138,26 +138,26 @@ export default [
             },
             fr: {
                     paragraphs : [ 
-                        "Julia is extremely bright. She has a deep capacity to learn and evaluate new things, and an aptitude for communicating that knowledge.", 
-                        "She has a wild imagination that knows no bounds, giving her a creative spirit that often takes her into fantasyland."
+                        "Julia est rigoureuse dans tout ce qu'elle entreprend. Certains la décrivent comme tendue et difficile à satisfaire, mais son opinion est tenue en haute estime.", 
+                        "Ses désirs sont forts. Certains la qualifieraient d'hédoniste en raison de son amour pour la décadence et la gratification instantanée."
                     ],
                     instructions : [ 
-                        "Remind her to cherish her perceptive wit, as it is a special, unique gift.", 
-                        "Share how in awe you are of her creative and imaginative talents.", 
-                        "Advise her to occasionally bring herself back to earth, to stay present, and find comfort in reality.", 
-                        "Make her feel like every new discovery is just around the corner."
+                        "Rappelez-lui que l'exigence peut être une vertu, et encouragez-la à faire confiance à ses instincts.", 
+                        "Montrez-lui que vous respectez sa façon de profiter des plaisirs de la vie.", 
+                        "Conseillez-lui de faire preuve d'un peu de retenue, parfois, et de trouver la beauté dans les moments les plus simples.", 
+                        "Assurez-lui qu'il pourra compter sur le soutien de son entourage aujourd'hui."
                     ]
             },
             de: {
                     paragraphs : [ 
-                        "Julia is extremely bright. She has a deep capacity to learn and evaluate new things, and an aptitude for communicating that knowledge.", 
-                        "She has a wild imagination that knows no bounds, giving her a creative spirit that often takes her into fantasyland."
+                        "Julia ist bei allem, was sie tut, äußerst sorgfältig. Manche meinen, sie sei schwer zufriedenzustellen, aber andere geben viel auf ihre Meinung.", 
+                        "Sie hat ausgeprägte Bedürfnisse. Aufgrund ihres Hangs nach Dekadenz und sofortiger Befriedigung würden sie manche als Hedonistin bezeichnen."
                     ],
                     instructions : [ 
-                        "Remind her to cherish her perceptive wit, as it is a special, unique gift.", 
-                        "Share how in awe you are of her creative and imaginative talents.", 
-                        "Advise her to occasionally bring herself back to earth, to stay present, and find comfort in reality.", 
-                        "Make her feel like every new discovery is just around the corner."
+                        "Erinnern Sie sie daran, dass Strenge eine Tugend sein kann, und ermutigen Sie sie dazu, ihren Instinkten zu folgen.", 
+                        "Zeigen Sie ihr, wie sehr Sie ihre Art respektieren, das Leben zu genießen.", 
+                        "Raten Sie ihr dazu, sich manchmal ein wenig zurückzuhalten und auch einfachere Augenblicke zu genießen.", 
+                        "Geben Sie ihr das Gefühl, dass sie heute andere unterstützen werden."
                     ]
             }
         },
